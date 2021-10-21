9b70ff3873512704cf0c2799d7f0bc6892fae03e
Matheus Victor <61367558+taotamat@users.noreply.github.com>
2021-10-21 07:58:29 -0300

    Criação das funções de remoções
    Modifiquei a estrutura do Nó pois estava impossível remover

---
+++ ajusta_cor.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <locale.h>
+#include <ctype.h>
+#include "arvore_rubro_negra.h"
+#include "ajusta_cor.h"
+#include "arquivo.h"
+#include "remocao.h"
+
+#define RED 1
+#define BLACK 0
+
+
+/*-------Rotações---------*/
+
+NO *rotacionaEsquerda(NO *A){
+    NO *B;
+    B = A->dir;
+    A->dir = B->esq;
+    B->esq = A;
+    B->cor = A->cor;
+    A->cor = RED;
+    return B; }
+
+NO *rotacionaDireita(NO *A){
+	NO *B;
+	B = A->esq;
+    A->esq = B->dir;
+    B->dir = A;
+    B->cor = A->cor;
+    A->cor = RED;
+    return B; }
+
+
+/* -    AJUSTES   - */
+
+NO *gestaoBALANCEAMENTO(ARVORE *arvore, NO **aux){
+
+    if(*aux != NULL){
+
+        if(cor((*aux)->dir) == RED)
+            *aux = rotacionaEsquerda(*aux);
+
+
+        if( (*aux)->esq != NULL && cor((*aux)->esq) == RED && cor((*aux)->esq->esq) == RED)
+            *aux = rotacionaDireita(*aux);
+
+
+        if( cor((*aux)->esq) == RED && cor((*aux)->dir) == RED )
+            trocaCor(*aux);
+
+
+        if( *aux == arvore->raiz && cor(*aux) == RED )
+            (*aux)->cor = BLACK;
+    
+    } else printf("eh null\n");
+        
+    return (*aux); }
+
+
---
+++ arvore_rubro_negra.h
@@ -0,0 +1,47 @@
+typedef struct NO_LISTA{
+    int nmr_linha; // linha onde a palavra se encontra no arquivo.
+    int nmr_ordem_linha; // posição onde a palavra se encontra na linha.
+    //FILE *posicao;
+    struct NO_LISTA *prox; // Proximo nó presente na lista.
+} NO_LISTA;
+
+typedef struct LISTA{
+    struct NO_LISTA *ini; // Nó inicial da lista.
+    struct NO_LISTA *fim; // Nó final da lista.
+} LISTA;
+
+typedef struct NO {
+    char palavra[50]; // String que armazena a palavra.
+    LISTA *lista_posicoes; // Lista de posições onde a palavra se encontra no texto.
+    int qnt_vezes; // Quantidade de vezes q a palavra aparece no texto.
+    struct NO *esq; // Filho da esquerda
+    struct NO *dir; // Filho da direita
+    int cor; // cor do nó
+} NO;
+
+typedef struct ARVORE {
+	NO *raiz; // Raiz da árvore
+	int tam; // Quantidade de Nós inseridos
+} ARVORE;
+
+// funções presentes em arvore_rubro_negra.c
+void ast();
+NO_LISTA *alocaNO_LISTA();
+LISTA *alocaLISTA();
+NO *alocaNO();
+ARVORE *alocaARV();
+int cor(NO *H);
+void trocaCor(NO *H);
+void apresentaCOR(int cor);
+void apresentaLISTA(NO_LISTA *aux);
+void apresentaNO(NO *aux);
+void preordem(NO *aux);
+void inordem(NO *aux);
+void posordem(NO *aux);
+int vazia(LISTA *lista);
+void insereLISTA(LISTA *lista, NO_LISTA *no);
+void inserir(ARVORE *arvore, NO **raiz, NO *novo);
+void ler(NO *no);
+void gestaoINSERCAO(ARVORE *arvore, char *palavra, int linha, int ordem);
+NO *buscar(NO *raiz, char *palavra, int *passos);
+void gestaoBUSCA(ARVORE *arvore);
